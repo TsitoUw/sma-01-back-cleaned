91eecad7a8cfd1d47fa391dc53070c3ac65152c6
TsitoUw <tsitohainanirinarak2@gmail.com>
2022-06-17 12:46:37 +0300

    fixed upload image

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -4,12 +4,7 @@ const imageUploader = require("./imageUploader");
 
 const uploadImage = [
   (req, res, next) => {
-    if (!req.file) {
-      next();
-      return;
-    }
     const id = req.params.id;
-    if (!directory) return errException();
     const basePath = path.join(__dirname, "../../", "static", "upload", id);
     imageUploader("photo", basePath)(req, res, next);
   },
